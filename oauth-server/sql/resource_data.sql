3bff6395f51dda11ff9de74ae2c9655fd0a8cfd0
George Alexiou <g.alexiou@pasiphae.eu>
2016-03-03 16:13:22 +0200

    oauth server removed

--- oauth-server/sql/resource_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO authority (id, authority) VALUES(0,'ROLE_OAUTH_ADMIN');
-INSERT INTO authority (id, authority) VALUES(1,'ROLE_ADMIN');
-INSERT INTO authority (id, authority) VALUES(2,'ROLE_USER');
-INSERT INTO credentials (id, enabled, name, password, version)  VALUES(1,1,'oauth_admin','admin',0);
-INSERT INTO credentials (id, enabled, name, password, version) VALUES(2,1,'resource_admin','admin',0);
-INSERT INTO credentials (id, enabled, name, password, version) VALUES(3,1,'user','user',0);
-INSERT INTO credentials_authorities (credentials_id, authorities_id) VALUES(1,1);
-INSERT INTO credentials_authorities (credentials_id, authorities_id) VALUES(2,2);
-INSERT INTO credentials_authorities (credentials_id, authorities_id) VALUES(3,3);
